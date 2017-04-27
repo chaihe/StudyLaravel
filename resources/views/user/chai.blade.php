ebf1550580c2e8f7bd8b515050a645fd500d3c81
Chai <chaichaichia@163.com>
2017-04-27 13:27:14 +0800

    模板条件判断     @if($first == 'chai')     @else     @endif

--- resources/views/user/chai.blade.php
+++ resources/views/user/chai.blade.php
@@ -3,8 +3,11 @@
 
 @section('content')
 
+    @if($first == 'chai')
     <h1>my name is Chai zhi  one more {{$first}} or {{$last}}</h1>
+    @else
     <h1>my age {{$age}}</h1>
+    @endif
 @stop
 
 @section('footer')
